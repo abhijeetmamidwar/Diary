aacca98e58575d57c494b3c27dc2424aa916d6e5
Abhijeet Mamidwar <abhijeetsm2001@gmail.com>
2021-07-28 18:46:16 +0530

    Modules reqests

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
+
+const passport = require("passport");
+const expressSession = require("express-session");
+const localStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const Account = require("./public/account");
+const activity = require("./public/function");
